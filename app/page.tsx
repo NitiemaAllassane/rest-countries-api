3df078ee4eb391ff77287ce8b714fbca4baf2f9d
Nitiema Allassane <allassanenitiema@gmail.com>
2026-06-28 21:27:08 +0000

    (update): Update home country grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default async function Home({ searchParams } : {
           </section>
           <section>
             {displayedCountries.length > 0 ? (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-12">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 md:gap-6  lg:gap-12">
                 {displayedCountries.map((country) => (
                   <CountryCard 
                     key={country.alpha3Code}
